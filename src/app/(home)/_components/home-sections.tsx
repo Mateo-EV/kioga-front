a477a8019aad65e044d44b6cb5cfa41cb5e0ac20
Mateo-EV <riojamatthew@gmail.com>
2024-05-13 23:21:13 -0500

    admin-pages-added

--- src/app/(home)/_components/home-sections.tsx
+++ src/app/(home)/_components/home-sections.tsx
@@ -51,7 +51,10 @@ export const StartSection = () => {
 
 export const WeekProductsSection = () => {
   return (
-    <section className="relative animate-fade-in overflow-hidden py-12 opacity-0 delay-700 fill-mode-forwards">
+    <section
+      className="relative animate-fade-in overflow-hidden py-12 opacity-0 fill-mode-forwards"
+      style={{ animationDelay: "850ms" }}
+    >
       <GradientDecorator className="left-1/2 size-[250px] -translate-x-1/2 -translate-y-1/2 xs:left-1/3 xs:top-[300px] xs:size-[300px]" />
       <div className="container xs:space-y-8">
         <div className="flex flex-col items-center justify-between gap-4 xs:flex-row">
---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,3 @@
+export default function AdmingLoginPage({}) {
+  return <div>AdmingLogin</div>;
+}
