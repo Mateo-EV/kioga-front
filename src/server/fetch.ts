38db98a1f87be1621813a159b407e65590b0afa4
Mateo-EV <riojamatthew@gmail.com>
2024-06-04 23:30:40 -0500

    origin-fixed

--- src/server/fetch.ts
+++ src/server/fetch.ts
@@ -7,8 +7,8 @@ type fetchParams = Parameters<typeof fetch>;
 export async function api<T>(url: string, init?: fetchParams[1]) {
   const headers = new Headers(headers_from_next());
   headers.set("Accept", "application/json");
-  headers.set("Origin", "http://localhost:3000");
-  headers.set("Referer", "http://localhost:3000/");
+  headers.set("Origin", env.NEXT_PUBLIC_APP_URL);
+  headers.set("Referer", env.NEXT_PUBLIC_APP_URL);
 
   const request = await fetch(env.NEXT_PUBLIC_BACKEND_URL + "/api" + url, {
     ...init,
